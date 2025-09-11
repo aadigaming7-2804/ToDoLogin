5b9d542916081eb68f2ddc14c3009779acf45045
aadigaming7-2804 <aadigaming.7@gmail.com>
2025-09-11 12:13:39 +0530

    User.js

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('./db');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  passwordResetRequest: { type: Boolean, default: false },
+  approvedForReset: { type: Boolean, default: false }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+module.exports = User;
